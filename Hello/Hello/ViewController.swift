df5016c7530145bc988866d6e0db22c9f66b18e4
Louis Ta <louistaa@gmail.com>
2021-04-05 11:35:35 -0700

    complete

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Louis Ta on 4/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "My First App!!", message: "Go Dawgs", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
